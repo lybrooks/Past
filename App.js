1cd12db3924f10d44b75dce1107f5eb1761ecc4e
lybrooks <506119809@qq.com>
2019-01-10 11:26:26 +0800

    add code-push

--- App.js
+++ App.js
@@ -8,7 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import codePush from 'react-native-code-push'
+const codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -18,11 +19,22 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+ componentDidMount(){
+  codePush.sync({
+    updateDialog: false,
+    installMode: codePush.InstallMode.IMMEDIATE,
+    mandatoryInstallMode:codePush.InstallMode.IMMEDIATE,
+    //deploymentKey为刚才生成的,用Platform判断下平台
+    deploymentKey: Platform.OS === 'ios'?'ke00zHdooD2aUUwLddS8RnKpOTD1a5fe1ca0-13e9-409f-941d-f964353f11e1':'tN_dvWkXKQxaUSv2sstiN6GpzKSea5fe1ca0-13e9-409f-941d-f964353f11e1',
+    });
+
+ }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>what are you talking about?</Text>
+        <Text style={styles.instructions}>Hello word aaaaaaaaaaaaaaa</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
---
+++ README.md
@@ -0,0 +1,20 @@
+// 
+
+
+
+ ## code-push 的 Deployment Key
+
+#### Android 平台
+
+- 推送命令 code-push release-react AndroidPast android -m -d Staging
+
+│ Production │ WqIhELs-bRLmAt-iVG4s3j-uPu2Ja5fe1ca0-13e9-409f-941d-f964353f11e1 │
+├────────────┼──────────────────────────────────────────────────────────────────┤
+│ Staging    │ tN_dvWkXKQxaUSv2sstiN6GpzKSea5fe1ca0-13e9-409f-941d-f964353f11e1 │
+#### iOS 平台
+
+- 推送命令 code-push release-react IOSPast ios -m -d Staging
+
+│ Production │ n5BODqU324Qk1LCaikpdK8XIvDNxa5fe1ca0-13e9-409f-941d-f964353f11e1 │
+├────────────┼──────────────────────────────────────────────────────────────────┤
+│ Staging    │ ke00zHdooD2aUUwLddS8RnKpOTD1a5fe1ca0-13e9-409f-941d-f964353f11e1 │
